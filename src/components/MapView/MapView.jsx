3109c79a793fa3f1b762de365628fd45c8d320cb
Christian Lara <christianramirezlara23@gmail.com>
2022-01-31 23:09:01 -0600

    Display a marker when the map is clicked

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -1,4 +1,7 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, MapConsumer } from 'react-leaflet';
+import L from 'leaflet';
+import icon from './icon';
+
 import 'leaflet/dist/leaflet.css';
 
 const MapView = ({ currentPosition }) => {
@@ -11,6 +14,15 @@ const MapView = ({ currentPosition }) => {
         url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       />
+      <MapConsumer>
+        {(map) => {
+          map.on('click', (e) => {
+            const { lat, lng } = e.latlng;
+            L.marker([lat, lng], { icon }).addTo(map);
+          });
+          return null;
+        }}
+      </MapConsumer>
     </MapContainer>
   );
 };
