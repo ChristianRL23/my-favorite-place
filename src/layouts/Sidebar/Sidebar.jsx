a75e9b624c8611f2d971b57ecd67920522921262
Christian Lara <christianramirezlara23@gmail.com>
2022-01-31 22:12:19 -0600

    Add welcome text to the navbar

--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -5,6 +5,17 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <img className="logo" src={logo} alt="App logo" />
+      <div className="sidebar__welcome">
+        <h2 className="sidebar__welcome__title">¡Bienvenid@!</h2>
+        <p className="sidebar__welcome__paragraph">
+          my favorite place es una aplicación que puedes usar para registar tus
+          lugares favoritos, tales como restaurantes, plazas, parques, etc.
+        </p>
+        <p className="sidebar__welcome__paragraph">
+          ¡Comienza usando el mapa para colocar un marcador en uno de tus
+          lugares favoritos!
+        </p>
+      </div>
     </aside>
   );
 };
