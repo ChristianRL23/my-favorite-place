385fcd9f7d2b784271d655f68b255b65eaac138e
Christian Lara <christianramirezlara23@gmail.com>
2022-01-31 23:24:55 -0600

    Change marker icon

--- src/components/MapView/icon.js
+++ src/components/MapView/icon.js
@@ -1,10 +1,11 @@
 import L from 'leaflet';
+import iconImg from './iconImg.png';
 
 const icon = L.icon({
-  iconSize: [25, 41],
+  iconSize: [27, 35],
   iconAnchor: [10, 41],
   popupAnchor: [2, -40],
-  iconUrl: 'https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png',
+  iconUrl: iconImg,
 });
 
 export default icon;
