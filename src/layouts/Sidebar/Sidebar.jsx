a899134bbc2b2692fbd645db1a55723063d9a88a
Christian Lara <christianramirezlara23@gmail.com>
2022-02-01 09:35:41 -0600

    Add form in the sidebar

--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -1,11 +1,16 @@
 import './Sidebar.scss';
 import logo from './logo.svg';
+import category from './category.svg';
+import favorite from './favorite.svg';
+import description from './description.svg';
+import name from './name.svg';
+import Input from '../../components/Input/Input';
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
       <img className="logo" src={logo} alt="App logo" />
-      <div className="sidebar__welcome">
+      {/* <div className="sidebar__welcome">
         <h2 className="sidebar__welcome__title">¡Bienvenid@!</h2>
         <p className="sidebar__welcome__paragraph">
           my favorite place es una aplicación que puedes usar para registar tus
@@ -15,6 +20,15 @@ const Sidebar = () => {
           ¡Comienza usando el mapa para colocar un marcador en uno de tus
           lugares favoritos!
         </p>
+      </div> */}
+      <div className="sidebar__add-place">
+        <h2 className="sidebar__add-place__title">Agregar un lugar</h2>
+        <form className="sidebar__add-place__form">
+          <Input icon={name} placeholder="Nombre del lugar" />
+          <Input icon={category} placeholder="Categoría" />
+          <Input icon={favorite} placeholder="¿Por qué es un lugar favorito?" />
+          <Input icon={description} placeholder="Descripción" textarea />
+        </form>
       </div>
     </aside>
   );
