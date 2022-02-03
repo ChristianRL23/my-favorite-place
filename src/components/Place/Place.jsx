9c56f2cb6a2554ed40315fa8b492d4046b090a2d
Christian Lara <christianramirezlara23@gmail.com>
2022-02-03 08:39:52 -0600

    Change styles of the place component

--- src/components/Place/Place.jsx
+++ src/components/Place/Place.jsx
@@ -5,7 +5,7 @@ const Place = ({ name, category, description, clickFn }) => {
     <div onClick={clickFn} className="place">
       <h6 className="place__name">{name}</h6>
       <h6 className="place__category">{category}</h6>
-      <p>{description}</p>
+      <p className="place__description">{description}</p>
     </div>
   );
 };
